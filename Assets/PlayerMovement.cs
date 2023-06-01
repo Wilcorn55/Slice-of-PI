6d5f36bc2cca38a1e6e2493d0c7f2bbee8fb0001
Wilcorn55 <blakewilburn@hotmail.com>
2023-06-01 23:21:06 +1200

    Safe

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,6 +11,20 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     Vector2 movement;
 
+    [SerializeField]
+
+    GameObject codePanel, closedSafe, openedSafe;
+
+    public static bool isSafeOpened = false;
+
+    void Start()
+    {
+        codePanel.SetActive(false);
+        closedSafe.SetActive(true);
+        openedSafe.SetActive(false);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -21,11 +35,35 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (isSafeOpened) {
+
+            codePanel.SetActive(false);
+            closedSafe.SetActive(false);
+            openedSafe.SetActive(true);
+
+        }
     }
 
     void FixedUpdate()
     {
         //Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime) ;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals ("Safe") && !isSafeOpened)
+        {
+            codePanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Safe"))
+        {
+            codePanel.SetActive(false);
+        }
     }
 }
