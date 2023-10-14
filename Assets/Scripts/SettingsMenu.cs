80451ee93da7108fb96e747a890ef5c20952e53a
Brittany <84743361+SourPanda13@users.noreply.github.com>
2023-10-14 18:30:04 +1300

    Fixed UI issues with the pause menu.

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 
 public class SettingsMenu : MonoBehaviour
 {
+    //This is the music mute toggle
+    public void muteToggle(bool muted)
+    {
+        if (muted)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
+
     //This will adjust the volume of the music through a slider
     public AudioMixer audioMixer;
 
